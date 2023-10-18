a905d0e1334c3226bbcd57c51509b395c670ad89
djtoler <84260551+djtoler@users.noreply.github.com>
2023-10-18 17:41:31 -0400

    edited ud name for ec2 app instances

--- user_data_5.1_java_python.sh
+++ user_data_5.1_java_python.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 #Download the Java, Python & AWS installation scripts
+sudo apt install -y default-jre
 curl -O https://raw.githubusercontent.com/djtoler/automated_installation_scripts/main/auto-python.sh
 curl -O https://raw.githubusercontent.com/djtoler/automated_installation_scripts/main/auto-aws_cli.sh
-sudo apt install -y default-jre
+
 
 #Make the scripts executable
 chmod +x auto-python.sh
